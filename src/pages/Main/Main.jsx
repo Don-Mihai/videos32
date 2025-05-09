2c802564f1be967de8d8a8451c4ef98b7e56b96e
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2025-05-09 08:31:02 +0300

    fix

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Main.module.css'; // Импортируем CSS-модуль
 
-const videos = [
-  { title: 'Видео 1', link: '/video/1' },
-  { title: 'Видео 2', link: '/video/2' },
-  { title: 'Видео 3', link: '/video/3' },
-  { title: 'Видео 4', link: '/video/4' },
-];
+const videos = [{ title: 'Начать просмотр', link: '/video/1' }];
 
 export default function Main() {
   const navigate = useNavigate();
@@ -18,9 +13,7 @@ export default function Main() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>
-        ВЫБЕРИТЕ РОЛИК, <br /> ЧТОБЫ ЗАПУСТИТЬ ЕГО НА ЭКРАНЕ
-      </h1>
+      <h1 className={styles.title}>ИСТОРИЯ НОВОРОССИИ</h1>
       <div className={styles.videos}>
         {videos.map((video, index) => (
           <button
